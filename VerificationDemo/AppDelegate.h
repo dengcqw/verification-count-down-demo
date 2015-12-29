d98028b56be4cc7ad68371e8fb85919333b86ec5
DengJinlong <dengjinlong@pingan.com.cn>
2015-12-29 14:48:57 +0800

    Initial Commit

---
+++ VerificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VerificationDemo
+//
+//  Created by DengJinlong on 12/29/15.
+//  Copyright © 2015 dengjinlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VerificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VerificationDemo
+//
+//  Created by DengJinlong on 12/29/15.
+//  Copyright © 2015 dengjinlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
